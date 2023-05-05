cbb479c8929e0a47063f96d2096f6e25b5abd8b4
John Michael Egana <johnmichael.egana@protonmail.com>
2023-05-05 14:12:28 +0800

    fix beast download filenames

--- install-beast.sh
+++ install-beast.sh
@@ -3,12 +3,12 @@
 beast_version="2.6.7"
 # uncomment when out of pre-release:
 #wget --quiet https://github.com/beast-dev/beast-mcmc/releases/download/v${beast_version}/BEASTv${beast_version}.tgz
-wget --quiet https://github.com/CompEvol/beast2/releases/download/v2.6.7/BEAST.v2.6.7.Linux.tgz -O BEASTv${beast_version}.tgz
+wget --quiet https://github.com/CompEvol/beast2/releases/download/v2.6.7/BEAST.v2.6.7.Linux.tgz
 
-tar -xzpf BEASTv${beast_version}.tgz
-rm BEASTv${beast_version}.tgz
+tar -xzpf BEASTv${beast_version}.Linux.tgz
+rm BEASTv${beast_version}.Linux.tgz
 
-mv BEASTv${beast_version}/bin/* /usr/local/bin
-mv BEASTv${beast_version}/lib/* /usr/local/lib
+mv BEASTv${beast_version}.Linux/bin/* /usr/local/bin
+mv BEASTv${beast_version}.Linux/lib/* /usr/local/lib
 
 beast -beagle_info
