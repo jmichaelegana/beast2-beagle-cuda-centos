90349d3a636e5cf234eba467643dc83bbc8690be
John Michael Egana <johnmichael.egana@protonmail.com>
2023-05-05 13:43:45 +0800

    install latest cmake manually

--- install-apt_packages.sh
+++ install-apt_packages.sh
@@ -4,13 +4,21 @@ set -e -o pipefail
 
 # Add some basics
 yum update -y
+
 yum install -y \
 	lsb-release ca-certificates wget rsync curl \
 	less nano vim git make \
 	dirmngr \
 	lz4 pigz bzip2 lbzip2 zstd \
-	cmake gcc gcc-c++ autoconf automake libtool git pkgconfig \
+	gcc gcc-c++ autoconf automake libtool git pkgconfig \
 	java-11-openjdk-devel
+
+cd /usr/local/src
+curl -LO https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.tar.gz
+tar -xvf cmake-3.26.3-linux-x86_64.tar.gz
+mv cmake-3.26.3-linux-x86_64 /usr/local/cmake
+echo 'export PATH="/usr/local/cmake/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
 mkdir -p /usr/local/cuda/bin
 
