8f151a4d87a77ea98bb34eff57b4989f9a032674
John Michael Egana <johnmichael.egana@protonmail.com>
2023-05-07 12:41:21 +0800

    beagle v4.0.0

--- install-beagle.sh
+++ install-beagle.sh
@@ -10,7 +10,7 @@ cd /opt/docker
 
 # beagle 3.1.2, known working with beast 1.10.5pre
 #git clone --depth=1 --branch="v3.1.2" https://github.com/beagle-dev/beagle-lib.git
-git clone --depth=1 --branch "v3.1.2" https://github.com/beagle-dev/beagle-lib.git
+git clone --depth=1 --branch "v4.0.0" https://github.com/beagle-dev/beagle-lib.git
 cd beagle-lib
 
 mkdir build
